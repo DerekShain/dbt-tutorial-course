50cbdf54dd3edf64886bb5e519e0c020fba4df24
Derek <derek0anthony@gmail.com>
2023-11-21 15:35:37 -0700

    advanced loops and macros

--- lessons/models/testing_jinja.sql
+++ lessons/models/testing_jinja.sql
@@ -3,14 +3,16 @@
 -- A comment that will be present
 
 {% set my_long_variable %}
-	SELECT 1 AS my_col
+	--SELECT 1 AS my_col
 {% endset %}
 
-{% set my_list = ['a', 'b', 'c'] %}
+{{ my_long_variable }}
+
+{% set my_list = ['1', '2', '3'] %}
 
 SELECT
 {% for item in my_list %}
 	{{ item }}{% if not loop.last %},{% endif %}
 {% endfor %}
 
-{{ my_list }}
+--{{ my_list }}
